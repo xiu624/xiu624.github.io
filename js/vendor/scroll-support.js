3fbe93f7dabf09eedf130660d4736425dfca8818
Fang Yuan <fayndee@hotmail.com>
2014-10-09 22:27:22 +0200

    Add smooth scroll for in page links. Adjust css. Change portal bg.

--- js/vendor/scroll-support.js
+++ js/vendor/scroll-support.js
@@ -234,3 +234,53 @@
 	};
 
 })(jQuery);
+
+/**
+ * Smooth scroll for in page links.
+ * http://wibblystuff.blogspot.de/2014/04/in-page-smooth-scroll-using-css3.html
+ */
+(function($) {
+
+    var target, scroll;
+
+    $.scrollSmoothInPage = function(duration) {
+        $("a[href*=#]:not([href=#])").on("click", function(e) {
+            if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+                target = $(this.hash);
+                target = target.length ? target : $("[id=" + this.hash.slice(1) + "]");
+
+                if (target.length) {
+                    if (typeof document.body.style.transitionProperty === 'string') {
+                        e.preventDefault();
+                  
+                        var avail = $(document).height() - $(window).height();
+
+                        scroll = target.offset().top;
+                  
+                        if (scroll > avail) {
+                            scroll = avail;
+                        }
+
+                        $("html").css({
+                            "margin-top" : ( $(window).scrollTop() - scroll ) + "px",
+                            "transition" : duration + "s ease-in-out"
+                        }).data("transitioning", true);
+                    } else {
+                        $("html, body").animate({
+                            scrollTop: scroll
+                        }, duration * 1000);
+                        return;
+                    }
+                }
+            }
+        });
+        $("html").on("transitionend webkitTransitionEnd msTransitionEnd oTransitionEnd", function (e) {
+            if (e.target == e.currentTarget && $(this).data("transitioning") === true) {
+                $(this).removeAttr("style").data("transitioning", false);
+                $("html, body").scrollTop(scroll);
+                return;
+            }
+        });
+    };
+
+})(jQuery);
